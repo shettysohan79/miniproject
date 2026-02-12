da8b883ace09d039e0de59719336604ee3db6c55
Sohan Shetty <shettysohan79@gmail.com>
2026-02-12 17:36:01 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+// Create room
+app.get("/create", (req, res) => {
+    const roomId = uuidv4();
+    res.redirect(`/room/${roomId}`);
+});
+
+// Room route
+app.get("/room/:roomId", (req, res) => {
+    res.sendFile(__dirname + "/public/room.html");
+});
+
+io.on("connection", (socket) => {
+
+    socket.on("joinRoom", (roomId) => {
+        socket.join(roomId);
+        socket.roomId = roomId;
+        console.log("User joined room:", roomId);
+    });
+
+    socket.on("chatMessage", (msg) => {
+        io.to(socket.roomId).emit("chatMessage", msg);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("Server running on port", PORT);
+});
